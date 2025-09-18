7f59950ee16fea99c25045043fd7f1d8ce27cd2b
Muneerali199 <alimuneerali245@gmail.com>
2025-09-18 10:05:28 +0530

    update: app/(tabs)/timetable-generator/ConflictResolutionScreen.tsx

---
+++ app/(tabs)/timetable-generator/ConflictResolutionScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function ConflictResolutionScreen() {
+  return (
+    <View>
+      <Text>Conflict Resolution Screen</Text>
+    </View>
+  );
+}
